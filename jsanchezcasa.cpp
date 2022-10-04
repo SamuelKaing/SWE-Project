9d7d2ccc201e4e08c239d47f1de9e53220cbabad
Student <student@lab-240-028.cs.csubak.edu>
2022-10-04 09:05:05 -0700

    Pause screenn added

--- jsanchezcasa.cpp
+++ jsanchezcasa.cpp
@@ -1,1 +1,30 @@
-//Testing
+//Juan Sanchez Casanova
+//Individual Source file
+#include "fonts.h"
+#include <GL/glx.h>
+unsigned int manage_state(unsigned int s)
+{
+    s = s^1;
+    return s;
+}
+void show_pause(int xres, int yres){
+        Rect r3;
+        r3.left = xres /2.3;
+	r3.bot = yres/1.8;
+       r3.center = 0;  
+       // int xcent = xres / 2;
+       // int ycent = yres / 2;
+       // int w = 200;
+       // glColor3f(0.0f, 0.0f, 0.0f);
+       // glBegin(GL_QUADS);
+       //         glVertex2f( xcent-w, ycent-w);
+       //         glVertex2f( xcent-w, ycent+w);
+       //         glVertex2f( xcent+w, ycent+w);
+       //        glVertex2f( xcent+w, ycent-w);  
+       //glEnd();        
+        
+        ggprint16(&r3, 40, 0x4e23838 , "PAUSE");
+	ggprint10(&r3, 40, 0x4e23838 , "Press 'p' to resume");
+
+}
+
---
+++ jsanchezcasa.h
@@ -0,0 +1,5 @@
+extern unsigned int manage_state(unsigned int s);
+
+
+
+void show_pause(int xres, int yres);
