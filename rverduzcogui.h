c207b1cd4efeba6319902fbed4f1a0fb7cf12a92
Student <student@lab-walkin-005.cs.csubak.edu>
2022-10-04 16:55:29 -0700

    gameover header_file

---
+++ rverduzcogui.h
@@ -0,0 +1,9 @@
+// Raul Verduzco 
+// Header file for gameover screen
+//
+//
+extern unsigned int manage_gameover_state(unsigned int g);
+
+extern void show_gameover_screen(int xres, int yres);
+
+
