0481961b00d95dc2b3591be0bd70370556eb9e32
Student <student@lab-240-029.cs.csubak.edu>
2022-10-05 09:06:12 -0700

    fixed indenting

--- skaing.cpp
+++ skaing.cpp
@@ -6,24 +6,24 @@
 
 void show_sam()
 {
-	printf("skaing\n");
+    printf("skaing\n");
 }
 
 unsigned int manage_help_state(unsigned int s)
 {
-	s = !s;
-	return s;
+    s = !s;
+    return s;
 }
 
 void show_controls(int xres, int yres)
 {
     Rect r2;
     r2.left = (xres / 2);
-   	r2.bot = yres / 1.8;
+    r2.bot = yres / 1.8;
     r2.center = 0;
 
-	//Outline box
-	int xcent = xres / 2;
+    //Outline box
+    int xcent = xres / 2;
     int ycent = yres / 2;
     int w = 180;
     glColor3f(0.9f, 1.0f, 1.07f);
@@ -32,7 +32,7 @@ void show_controls(int xres, int yres)
     	glVertex2f( xcent-w, ycent+w);
     	glVertex2f( xcent+w, ycent+w);
     	glVertex2f( xcent+w, ycent-w);
-	glEnd();
+    glEnd();
 
     w = w - 2;
     glColor3f(0.0f, 0.0f, 0.0f);
@@ -41,14 +41,14 @@ void show_controls(int xres, int yres)
     	glVertex2f( xcent-w, ycent+w);
     	glVertex2f( xcent+w, ycent+w);
     	glVertex2f( xcent+w, ycent-w);
-	glEnd();
+    glEnd();
 
-	r2.left = (xres / 2) - (w / 4);
-	r2.bot = (yres / 2) + (w / 3);
+    r2.left = (xres / 2) - (w / 4);
+    r2.bot = (yres / 2) + (w / 3);
 
-	ggprint16(&r2, 40, 0x2E281, "Controls");
-	ggprint10(&r2, 40, 0x2e281, "Movement -- Arrow Keys");
-	ggprint10(&r2, 40, 0x2e281, "Shoot -- Spacebar");
-	ggprint10(&r2, 40, 0x2e281, "Pause -- p");
-	ggprint10(&r2, 40, 0x2e281, "Credits -- c");
+    ggprint16(&r2, 40, 0x2E281, "Controls");
+    ggprint10(&r2, 40, 0x2e281, "Movement -- Arrow Keys");
+    ggprint10(&r2, 40, 0x2e281, "Shoot -- Spacebar");
+    ggprint10(&r2, 40, 0x2e281, "Pause -- p");
+    ggprint10(&r2, 40, 0x2e281, "Credits -- c");
 }
