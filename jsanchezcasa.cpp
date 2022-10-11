731b5d8dd29debc939796e08dd1cfc5926620703
jsanchez1801 <111897337+jsanchez1801@users.noreply.github.com>
2022-10-11 09:28:38 -0700

    added transparent pause screen

--- jsanchezcasa.cpp
+++ jsanchezcasa.cpp
@@ -11,17 +11,24 @@ void show_pause(int xres, int yres){
         Rect r3;
         r3.left = xres /2.3;
 	r3.bot = yres/1.8;
-       r3.center = 0;  
-       // int xcent = xres / 2;
-       // int ycent = yres / 2;
-       // int w = 200;
-       // glColor3f(0.0f, 0.0f, 0.0f);
-       // glBegin(GL_QUADS);
-       //         glVertex2f( xcent-w, ycent-w);
-       //         glVertex2f( xcent-w, ycent+w);
-       //         glVertex2f( xcent+w, ycent+w);
-       //        glVertex2f( xcent+w, ycent-w);  
-       //glEnd();        
+        r3.center = 0;  
+        int xcent = xres ;
+        int ycent = yres ;
+       
+        
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+	glColor4f(0.0f, 0.0f, 0.0f, 0.5);
+        glBegin(GL_QUADS);
+                
+		glVertex2f( -xcent, -ycent);
+                glVertex2f( -xcent,  ycent);
+                glVertex2f(  xcent,  ycent);
+                glVertex2f(  xcent, -ycent);  
+
+	
+	glEnd();
+ 	glDisable(GL_BLEND);       
         
         ggprint16(&r3, 40, 0x4e23838 , "PAUSE");
 	ggprint10(&r3, 40, 0x4e23838 , "Press 'p' to resume");
