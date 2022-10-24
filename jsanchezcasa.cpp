1dcae318038cf15c4cdbc43a60b9fa766b1e7c62
jsanchez1801 <111897337+jsanchez1801@users.noreply.github.com>
2022-10-24 09:40:02 -0700

    Update to personal file 10/24

--- jsanchezcasa.cpp
+++ jsanchezcasa.cpp
@@ -2,6 +2,7 @@
 //Individual Source file
 #include "fonts.h"
 #include <GL/glx.h>
+
 unsigned int manage_state(unsigned int s)
 {
     s = s^1;
@@ -34,4 +35,30 @@ void show_pause(int xres, int yres){
 	ggprint10(&r3, 40, 0x4e23838 , "Press 'p' to resume");
 
 }
+void juanfeature(int xres, int yres){
+    	 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+         glEnable(GL_BLEND);
 
+         glColor3f(1.0, 0.5, 0.5);
+         glColor4f(1.0, 0.5, 0.5, 0.5);
+         int w = 40;
+         glBegin(GL_TRIANGLE_STRIP);
+         
+	 	glVertex2i(0, 0);
+         	glVertex2i(0+w, w);
+
+         	glVertex2i(0, yres);
+         	glVertex2i(0+w, yres-w);
+
+         	glVertex2i(xres, yres);
+         	glVertex2i(xres-w, yres-w);
+
+         	glVertex2i(xres, 0);
+         	glVertex2i(xres-w, w);
+
+         	glVertex2i(0,0);
+         	glVertex2i(0+w, w);
+
+         glEnd();
+         glDisable(GL_BLEND);
+}
--- jsanchezcasa.h
+++ jsanchezcasa.h
@@ -3,3 +3,5 @@ extern unsigned int manage_state(unsigned int s);
 
 
 void show_pause(int xres, int yres);
+
+void juanfeature(int xres, int yres);
